3eef13cc34a6f29314750f8ccc979e090bb91f3e
Starinin Andrey <starininandrey@gmail.com>
2022-09-01 18:31:31 +0300

    Init

---
+++ CollectionWPFDemo.App/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CollectionWPFDemo.App;
+
+public partial class MainWindow : Window
+{
+    private List<string> _list;
+
+    public MainWindow()
+    {
+        _list = new List<string> { "first", "second", "last" };
+
+        InitializeComponent();
+
+        ListBox.ItemsSource = _list;
+    }
+
+    private void ListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var item = ListBox.SelectedItem;
+        MessageBox.Show(item.ToString());
+    }
+}
